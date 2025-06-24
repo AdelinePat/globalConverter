077e1bfd863f5540171231a71105c25c383131ba
Adeline <adeline.patenne@hotmail.fr>
2025-06-24 13:26:51 +0200

    feat: handling forbidden character done

--- src/ascii/CleanInput.java
+++ src/ascii/CleanInput.java
@@ -1,4 +1,5 @@
 package ascii;
+import custom_exceptions.UserError;
 
 public class CleanInput {
     public String clean_input;
@@ -11,14 +12,16 @@ public class CleanInput {
     private boolean isValidInput(String raw_user_input) {
         return raw_user_input.matches(".*[^a-zA-Z0-9 ].*");
     }
-    public void getCleanInput(String raw_user_input) {
-        boolean vatefairefoutre = this.isValidInput(raw_user_input);
 
-        if (vatefairefoutre) {
-            System.out.println("Mdr, y'a des caractères pas accepté");
-        } else {
-            raw_user_input = raw_user_input.trim();
-            clean_input = raw_user_input;
-        }
+    public void getCleanInput(String raw_user_input) throws UserError {
+            if (this.isValidInput(raw_user_input)) {
+                System.out.println("Mdr, y'a des caractères pas accepté");
+                throw new UserError("Des caractères non autorisés ont été utilisé : veuillez utiliser uniquement des " +
+                        "lettres (sans accent) et des chiffres.");
+            } else {
+                raw_user_input = raw_user_input.trim();
+                clean_input = raw_user_input;
+            }
+
     }
 }
--- src/custom_exceptions/AlgorithmError.java
+++ src/custom_exceptions/AlgorithmError.java
@@ -1,4 +1,4 @@
-package exceptions;
+package custom_exceptions;
 
 public class AlgorithmError extends Exception {
     public AlgorithmError(String message) {
