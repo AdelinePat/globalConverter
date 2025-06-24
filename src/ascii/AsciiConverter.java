28d2b814dc17332c33e7d858926f547b6e4745ca
Adeline <adeline.patenne@hotmail.fr>
2025-06-24 13:50:58 +0200

    fix: Algorithm Error throw in loop cause crash, fix by using boolean

--- src/ascii/AsciiConverter.java
+++ src/ascii/AsciiConverter.java
@@ -6,6 +6,7 @@
     import java.util.Map;
     import com.google.gson.Gson;
     import com.google.gson.reflect.TypeToken;
+    import custom_exceptions.AlgorithmError;
 
     public class AsciiConverter {
         public Map<String, Integer> ascii_map;
@@ -51,16 +52,23 @@
         }
         public List<String> fromIntToString(List<List<Integer>> user_input_int) {
             List<String> result = new ArrayList<>();
-
-            for (List<Integer> word : user_input_int) {
+            try {
+                for (List<Integer> word : user_input_int) {
 //            System.out.println(word);
-                StringBuilder ascii_string = new StringBuilder();
-                for (Integer character : word) {
-                    String letter = this.getCharacterByAsciiValue(character);
-                    System.out.print(letter);
+                    StringBuilder ascii_string = new StringBuilder();
+                    for (Integer character : word) {
+                        String letter = this.getCharacterByAsciiValue(character);
+                        System.out.print(letter);
+//                        Test exception AlgorithmError
+//                        String letter_test = this.getCharacterByAsciiValue(156);
+//                        System.out.print(letter_test);
+                    }
+                    System.out.print(" ");
                 }
-                System.out.print(" ");
+            } catch (AlgorithmError e) {
+                System.out.println("\u001B[31mErreur d'Algorithme : \u001B[0m" + e.getMessage());
             }
+
 
             // debug
             System.out.println();
@@ -73,13 +81,19 @@
             return result;
         }
 
-        private String getCharacterByAsciiValue(Integer ascii_value) {
+        private String getCharacterByAsciiValue(Integer ascii_value) throws AlgorithmError {
+            boolean isValueFound = false;
             for (Map.Entry<String, Integer> entry : ascii_map.entrySet()) {
                 if (entry.getValue().equals(ascii_value)) {
+                    isValueFound = true;
                     return entry.getKey();
                 }
             }
-            return "?"; // Return a placeholder if not found
+            if (!isValueFound) {
+                throw new AlgorithmError("Attention, la valeur ASCII n'a pas de clé" +
+                        "correspondante dans le ascii_table.json.");
+            }
+            return ""; // should find a way not to have a return here ???
         }
 
     }
