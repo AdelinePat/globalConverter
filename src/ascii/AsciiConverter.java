17be156325c4d2fd88e83ab0040f2ea2bde59a34
Adeline <adeline.patenne@hotmail.fr>
2025-06-24 11:40:18 +0200

    feat: CleanInput class that must be implemented + letter into ascii int
    test in main for now, maybe create another class to manage data in a more efficient and clean manner

--- src/ascii/AsciiConverter.java
+++ src/ascii/AsciiConverter.java
@@ -1,26 +1,52 @@
-package ascii;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Map;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+    package ascii;
+    import java.io.InputStream;
+    import java.io.InputStreamReader;
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.Map;
+    import com.google.gson.Gson;
+    import com.google.gson.reflect.TypeToken;
 
-public class AsciiConverter {
-    private Map<String, Integer> ascii_map;
-    public AsciiConverter() {
-        loadAsciiTable();
-    }
-    private void loadAsciiTable() {
-
-        InputStream inputStream = getClass().getResourceAsStream("/ascii_table.json");
-
-        if (inputStream == null) {
-            System.out.println("File not found! Please check the path.");
-            return; // Exit or handle the error
+    public class AsciiConverter {
+        public Map<String, Integer> ascii_map;
+        public AsciiConverter() {
+            loadAsciiTable();
         }
-        InputStreamReader reader = new InputStreamReader(inputStream);
-        Gson gson = new Gson();
-        ascii_map = gson.fromJson(reader, new TypeToken<Map<String, Integer>>() {}.getType());
-        System.out.println("ASCII Table loaded:" + ascii_map);
+        private void loadAsciiTable() {
+
+            InputStream inputStream = getClass().getResourceAsStream("/ascii_table.json");
+
+            if (inputStream == null) {
+                System.out.println("File not found! Please check the path.");
+                return; // Exit or handle the error
+            }
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            Gson gson = new Gson();
+            ascii_map = gson.fromJson(reader, new TypeToken<Map<String, Integer>>() {}.getType());
+            System.out.println("ASCII Table loaded:" + ascii_map);
+        }
+
+        public List<List<Integer>> parseStringIntoInt(String user_input) {
+            List<List<Integer>> result = new ArrayList<>();
+            String[] words = user_input.trim().split(" ");
+
+            for (String word : words) {
+                List<Integer> ascii_values = new ArrayList<>();
+                for (char c : word.toCharArray()) {
+                    String character = String.valueOf(c);
+//                    System.out.println("valueof(c) :" + character + " ");
+                    Integer ascii_code = ascii_map.get(character);
+//                    System.out.println("ascii_map.get(character) :" + ascii_code + " ");
+
+                    if (ascii_code != null) {
+                        ascii_values.add(ascii_code);
+                    } else {
+                        System.out.println("ascii_code is null or not found inside ascii_map");
+                    }
+                }
+                result.add(ascii_values);
+                System.out.println(word + "\n");
+            }
+            return result;
+        }
     }
-}
---
+++ src/ascii/CleanInput.java
@@ -0,0 +1,8 @@
+package ascii;
+
+public class CleanInput {
+    public String clean_input;
+    /* Implementation of regex, clean spaces at begining and
+    end of the string, make sure the string doesn't contain anything more than alphanumeric character
+    If any exception are thrown : please use UserError custom exception */
+}
