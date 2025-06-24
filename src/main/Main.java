08191b67ee9a73bf36f9ca8da17574ff10522021
Adeline <adeline.patenne@hotmail.fr>
2025-06-24 13:13:12 +0200

    feat: checks for forbidden characters
    lacks of exceptions management causes crash

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 import ascii.AsciiConverter;
+import ascii.CleanInput;
 import converters.HexaConverter;
 import converters.IConverter;
 import converters.BinaryConverter;
@@ -13,19 +14,15 @@ public class Main {
         IConverter binary_converter = new BinaryConverter();
         IConverter octal_converter = new OctalConverter();
         IConverter hexa_converter  = new HexaConverter();
-        System.out.println("J'en ai ras le bol de IntelliJ qui est vraiment trop CON");
         AsciiConverter ascii_converter = new AsciiConverter();
+        CleanInput clean_input = new CleanInput();
+
+        clean_input.getCleanInput("Salut les gens ca va bien ou quoi");
 
         List<List<Integer>> ras_le_bol = new ArrayList<>();
-        ras_le_bol = ascii_converter.parseStringIntoInt("Coucou les gens");
-
-        for (List<Integer> word : ras_le_bol) {
-//            System.out.println(word);
-            for (Integer character : word) {
-                System.out.print(character + " ");
-            }
-            System.out.println();
-        }
+        List<String> jenaimarre = new ArrayList<>();
+        ras_le_bol = ascii_converter.parseStringIntoInt(clean_input.clean_input);
+        jenaimarre = ascii_converter.fromIntToString(ras_le_bol);
 
     }
 }
