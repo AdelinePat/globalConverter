042d6d724eb0f47f93e50940cfd5c54a55a5def4
Adeline <adeline.patenne@hotmail.fr>
2025-06-26 13:33:10 +0200

    fix: parsing refactoring and fixing in progress

---
+++ src/ascii/Parsing.java
@@ -0,0 +1,44 @@
+package ascii;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Parsing {
+    public static List<String> splitSentenceIntoWords(String user_input) {
+        return new ArrayList<>(Arrays.asList(user_input.trim().split(" ")));
+    }
+
+    public static List<String> splitWordIntoLetters(String word) {
+        List<String> letters = new ArrayList<>();
+        for (char c : word.toCharArray()) {
+            letters.add(String.valueOf(c));
+        }
+        return letters;
+    }
+
+    public static List<List<String>> splitSentenceIntoLetterGroups(String user_input) {
+        List<List<String>> result = new ArrayList<>();
+        for (String word: splitSentenceIntoWords(user_input)) {
+            result.add(splitWordIntoLetters(word));
+        }
+        return result;
+    }
+
+    public static List<List<Integer>> parseGroupNumbers(String user_input) {
+        List<List<Integer>> result = new ArrayList<>();
+        String[] words = user_input.split(" {2}");
+        for (String word: words) {
+            List<Integer> numbers = new ArrayList<>();
+            String[] parts = word.trim().split(" ");
+
+            for (String part : parts) {
+                if (!part.isEmpty()) {
+                    numbers.add(Integer.parseInt(part));
+                }
+            }
+            result.add(numbers);
+        }
+        return result;
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 import ascii.AsciiUtils;
+import ascii.Parsing;
 import ascii.CleanInput;
 import caesar.CaesarCipher;
 import converters.*;
@@ -20,23 +21,28 @@ public class Main {
 
         try {
             String original_user_input = clean_input.getCleanInput("Vas y Jolyne tu geres");
+            System.out.println("Split original string : " + Parsing.splitSentenceIntoLetterGroups(original_user_input));
             String user_input_int = decimal_converter.conversion(original_user_input);
-            String user_input_bin =  binary_converter.conversion(user_input_int);
+            String user_input_int_reversed = decimal_converter.reverseConversion(user_input_int);
+            System.out.println("ascii vers int : " + user_input_int);
+            System.out.println("int vers ascii ? " + user_input_int_reversed);
+            System.out.println("parsing en groupe de nombre : " + Parsing.parseGroupNumbers(user_input_int));
+//            String user_input_bin =  binary_converter.conversion(user_input_int);
+//
+//            String user_caesar = caesar_cipher.caesarEncrypt(user_input_int, 3);
+//            String user_caesar_reversed = caesar_cipher.caesarEncrypt(user_caesar);
+//            String user_readable_caesar = decimal_converter.reverseConversion(user_caesar);
+//            String user_input_string = decimal_converter.reverseConversion(user_caesar_reversed);
+//
+//            String user_bin_to_int = binary_converter.reverseConversion(user_input_bin);
 
-            String user_caesar = caesar_cipher.caesarEncrypt(user_input_int, 3);
-            String user_caesar_reversed = caesar_cipher.caesarEncrypt(user_caesar);
-            String user_readable_caesar = decimal_converter.reverseConversion(user_caesar);
-            String user_input_string = decimal_converter.reverseConversion(user_caesar_reversed);
-
-            String user_bin_to_int = binary_converter.reverseConversion(user_input_bin);
-
-            System.out.println("Entrée input d'entrée (num)      : " + user_input_int);
-            System.out.println("Entrée input bin (num)           : " + user_input_bin);
-            System.out.println("Entrée après césar (num)         : " + user_caesar);
-            System.out.println("Entrée après césar (num)         : " + user_readable_caesar);
-            System.out.println("Entrée après césar inverse (num) : " + user_caesar_reversed);
-            System.out.println("Entrée input de base (char)      : " + user_input_string);
-            System.out.println("Entrée bin to int (num)          : " + user_bin_to_int);
+//            System.out.println("Entrée input d'entrée (num)      : " + user_input_int);
+//            System.out.println("Entrée input bin (num)           : " + user_input_bin);
+//            System.out.println("Entrée après césar (num)         : " + user_caesar);
+//            System.out.println("Entrée après césar (num)         : " + user_readable_caesar);
+//            System.out.println("Entrée après césar inverse (num) : " + user_caesar_reversed);
+//            System.out.println("Entrée input de base (char)      : " + user_input_string);
+//            System.out.println("Entrée bin to int (num)          : " + user_bin_to_int);
 
         }
         catch (UserError e) {
