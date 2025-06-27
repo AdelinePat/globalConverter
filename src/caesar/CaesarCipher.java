a841cba80af7626a21e08389fb229772d69481b6
jolyne-mangeot <jolyne.mangeot@gmail.com>
2025-06-27 13:08:50 +0200

    progress: patch minor issues such as caesar encryption method calling and handling user input mismatch

--- src/caesar/CaesarCipher.java
+++ src/caesar/CaesarCipher.java
@@ -13,7 +13,7 @@ public class CaesarCipher {
     }
 
     public String caesarEncrypt(String user_input_int, int offset) {
-        List<List<Integer>> convertedInput = AsciiUtils.parseStringIntoIntList(user_input_int);
+        List<List<Integer>> initialDecimalValues = AsciiUtils.parseStringIntoIntList(user_input_int);
 
         if (offset != 0) {
             key = offset;
@@ -22,45 +22,46 @@ public class CaesarCipher {
         }
         List<List<Integer>> result = new ArrayList<>();
 
-        for (int list_index = 0; list_index < convertedInput.size(); list_index++) {
+        for (List<Integer> integers_group : initialDecimalValues) {
 
             List<Integer> word_result = new ArrayList<>();
 
-            for (int value_index = 0; value_index < convertedInput.get(list_index).size(); value_index++) {
-
-                int new_value = convertedInput.get(list_index).get(value_index);
-                int min_ascii = 0, max_ascii = 0;
-
-                if (48 <= new_value && new_value <= 57) {
-                    min_ascii = 48;
-                    max_ascii = 57;
-                }
-                else if (65 <= new_value && new_value <= 90) {
-                    min_ascii = 65;
-                    max_ascii = 90;
-                }
-                else if (97 <= new_value && new_value <= 122) {
-                    min_ascii = 97;
-                    max_ascii = 122;
-                }
-
-                new_value += this.key;
-
-                if (new_value < min_ascii) {
-                    while (new_value < min_ascii) {
-                        new_value += max_ascii - min_ascii + 1;
-                    }
-                }
-                else if (new_value > max_ascii) {
-                    while (new_value > max_ascii) {
-                        new_value -= max_ascii - min_ascii + 1;
-                    }
-                }
-
-                word_result.add(new_value);
+            for (int new_value : integers_group) {
+                word_result.add(getNewValue(new_value));
             }
             result.add(word_result);
         }
         return AsciiUtils.concatenateFromInt(result);
+    }
+
+    private int getNewValue(int new_value) {
+        int min_ascii = 0, max_ascii = 0;
+
+        if (48 <= new_value && new_value <= 57) {
+            min_ascii = 48;
+            max_ascii = 57;
+        }
+        else if (65 <= new_value && new_value <= 90) {
+            min_ascii = 65;
+            max_ascii = 90;
+        }
+        else if (97 <= new_value && new_value <= 122) {
+            min_ascii = 97;
+            max_ascii = 122;
+        }
+
+        new_value += this.key;
+
+        if (new_value < min_ascii) {
+            while (new_value < min_ascii) {
+                new_value += max_ascii - min_ascii + 1;
+            }
+        }
+        else if (new_value > max_ascii) {
+            while (new_value > max_ascii) {
+                new_value -= max_ascii - min_ascii + 1;
+            }
+        }
+        return new_value;
     }
 }
