58c7f79f2cbd6059b3c688dce0d8319c371dc284
jolyne-mangeot <jolyne.mangeot@gmail.com>
2025-06-27 11:49:44 +0200

    progress: functional interactive user interface, missing user error handling

--- src/main/InteractiveMain.java
+++ src/main/InteractiveMain.java
@@ -8,13 +8,14 @@ public class InteractiveMain {
     static UserInterfaceControl current_user;
     static UserInterfaceView user_interface_view;
 
-    static String initial_input;
-    static String ascii_string;
-    static String previous_conversion;
-    static String conversion_result;
+    static String initial_input = "";
+    static String ascii_string = "";
+    static String previous_conversion = "";
+    static String conversion_result = "";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AlgorithmError {
         startProgram();
+        System.out.println(runProgram());
 
 
     }
@@ -37,9 +38,28 @@ public class InteractiveMain {
         }
     }
 
-    private static int runProgram(String initial_input) {
+    private static int runProgram() throws AlgorithmError {
         while (true) {
-            break;
+            user_interface_view.printConversions(initial_input, previous_conversion, conversion_result);
+            user_interface_view.printSelectionMenu();
+            int user_choice = user_interface_view.handleUserIntChoice();
+            if (conversion_result != "") {
+                previous_conversion = conversion_result;
+            }
+            if (user_choice == 1) {
+                conversion_result = ascii_string;
+            }
+            else if (user_choice != 5) {
+                conversion_result = current_user.convertUserChoice(user_choice, ascii_string);
+            }
+            else if (user_choice == 5) {
+                user_interface_view.printCaesarEncrypting();
+                int caesar_offset = user_interface_view.handleUserIntChoice();
+                conversion_result = current_user.convertUserChoice(5, caesar_offset);
+            }
+            else {
+                break;
+            }
         }
         return 0;
     }
--- src/user_interface/UserInterfaceControl.java
+++ src/user_interface/UserInterfaceControl.java
@@ -31,21 +31,21 @@ public class UserInterfaceControl {
     public String convertUserChoice(int choice_index, String conversion_base) throws AlgorithmError {
         return switch (choice_index) {
             case 1 -> decimal_converter.conversion(conversion_base);
-            case 2 -> decimal_converter.reverseConversion(conversion_base);
+//            case 6 -> decimal_converter.reverseConversion(conversion_base);
+            case 2 -> binary_converter.conversion(conversion_base);
+//            case 7 -> binary_converter.reverseConversion(conversion_base);
             case 3 -> octal_converter.conversion(conversion_base);
-            case 4 -> octal_converter.reverseConversion(conversion_base);
-            case 5 -> binary_converter.conversion(conversion_base);
-            case 6 -> binary_converter.reverseConversion(conversion_base);
-            case 7 -> hexa_converter.conversion(conversion_base);
-            case 8 -> hexa_converter.reverseConversion(conversion_base);
-            case 10 -> caesar_cipher.caesarEncrypt(conversion_base);
+//            case 8 -> octal_converter.reverseConversion(conversion_base);
+            case 4 -> hexa_converter.conversion(conversion_base);
+//            case 9 -> hexa_converter.reverseConversion(conversion_base);
+//            case 10 -> caesar_cipher.caesarEncrypt(conversion_base);
             default -> "";
         };
     }
 
-    String convertUserChoice(int choice_index, int caesar_offset) throws AlgorithmError {
+    public String convertUserChoice(int choice_index, int caesar_offset) throws AlgorithmError {
         return switch (choice_index) {
-            case 9 -> ascii_string = caesar_cipher.caesarEncrypt(ascii_string, caesar_offset);
+            case 5 -> ascii_string = caesar_cipher.caesarEncrypt(ascii_string, caesar_offset);
             default -> "";
         };
     }
